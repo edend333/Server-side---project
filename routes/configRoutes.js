f81e492465a352e8739952531e15ecc3176140d7
Eden D <99354718+edend333@users.noreply.github.com>
2023-03-18 16:49:44 +0200

    Update configRoutes.js

--- routes/configRoutes.js
+++ routes/configRoutes.js
@@ -2,9 +2,7 @@ const usersR = require("./users");
 const toyR = require("./toy");
 
 
-
 exports.routesInit = (app) => {
-  app.use("/",indexR);
   app.use("/users",usersR);
   app.use("/toy",toyR);
 
